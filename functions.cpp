3bf57e2a834283617e6493fba93b4c0dd5ac439a
Ayush Vardhan <ayushvardhan08538@gmail.com>
2025-07-24 10:23:56 +0530

    Initial commit: DSA practice

---
+++ functions.cpp
@@ -0,0 +1,70 @@
+/*#include<bits/stdc++.h>
+using namespace std;
+void myfunction(){
+    cout<<"ayush is selmon";
+}
+int main()
+{
+     myfunction();
+     cout<<endl;
+     myfunction();
+     cout<<endl;
+     myfunction();
+     return 0;
+}
+     */
+
+
+/*#include<bits/stdc++.h>
+using namespace std;
+void myfunction(int num){
+    cout<<num<<endl;
+}
+int main(){
+    myfunction(1);
+    myfunction(2);
+    myfunction(3);
+    return 0;
+}*/
+
+
+/*#include<bits/stdc++.h>
+using namespace std;
+void myfunction(string name){
+    cout<<name<<endl;
+}
+int main(){
+    myfunction("Ayush");
+    myfunction("selmon");
+    myfunction("shera");
+    return 0;
+}*/
+
+/*#include<bits/stdc++.h>
+using namespace std;
+int  myfunction(int x){
+    return 5+x;
+}
+int main(){
+    cout<<myfunction(5);
+    return 0;
+}*/
+
+
+/*#include<bits/stdc++.h>
+using namespace std;
+int myfunction(int x,int y){
+return 10+x+y;
+}
+int main(){
+    cout<<myfunction(10,10);
+    return 0;
+}*/
+
+
+
+
+
+
+
+
